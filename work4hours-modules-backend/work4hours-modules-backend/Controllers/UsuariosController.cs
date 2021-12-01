5f23fe7ae2c8a80e47cc44015a0ecfefa4d9864f
David Torres <dmtg16@hotmail.com>
2021-12-01 15:04:28 -0500

    Se añade metodo de validacion de existencia de un usuario

--- work4hours-modules-backend/work4hours-modules-backend/Controllers/UsuariosController.cs
+++ work4hours-modules-backend/work4hours-modules-backend/Controllers/UsuariosController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using work4hours_modules_backend.Models;
@@ -16,6 +17,19 @@ namespace work4hours_modules_backend.Controllers
     {
         BaseDatos bd = new BaseDatos();
         
+        private bool UserExist(string email)
+        {
+            DataTable response = bd.getTable($"select * from usuarios where correo = '{email}' ");
+
+            if (response == null)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+
         // GET: api/<UsuariosController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -34,9 +48,19 @@ namespace work4hours_modules_backend.Controllers
         [HttpPost]
         public string Post([FromBody] Usuarios user)
         {
-            string sql = $"INSERT INTO usuarios (nombres, apellidos, celular, correo, contrasenna, fnac) VALUES ('{user.nombres}','{user.apellidos}','{user.celular}','{user.correoElectronico}','{Seguridad.Encriptar(user.contrasenna)}','{user.fecNac}')";
-            string result = bd.ejecutarSQL(sql);
+            string result = "";
+
+            if (UserExist(user.correoElectronico))
+            {
+                string sql = $"INSERT INTO usuarios (nombres, apellidos, celular, correo, contrasenna, fnac) VALUES ('{user.nombres}','{user.apellidos}','{user.celular}','{user.correoElectronico}','{Seguridad.Encriptar(user.contrasenna)}','{user.fecNac}')";
+                result = bd.ejecutarSQL(sql);
+            }
+            else
+            {
+                result = "El usuario ya existe";
+            }
             return result;
+
         }
 
         // PUT api/<UsuariosController>/5
